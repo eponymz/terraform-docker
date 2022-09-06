cedff98953a4dfeb2eba4232f18261674d857286
Ian Sabey <ian.sabey@edquity.co>
2022-09-06 09:37:43 -0600

    INF-404 | create gitconfig file

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@ set -e
 
 if [ -n "${GO_COMMON_USER}" ]; then
   echo "Configuring GO_COMMON_USER: $GO_COMMON_USER"
+  touch $HOME/.gitconfig
   git config --global url."https://${GO_COMMON_USER}:${GO_COMMON_PASS}@gitlab.com".insteadOf "https://gitlab.com"
 else
   echo "WARNING: GO_COMMON_USER credentials are not set"
 fi
 
 go install . 
-rm .gitconfig
+rm $HOME/.gitconfig
